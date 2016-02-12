f0bfba96a8475e79b801ace830cb54582a59a7c5
綿貫岳海 <takemi007@hotmail.com>
2016-02-13 02:17:20 +0900

    +++

--- TouchUpNum/TopViewController.swift
+++ TouchUpNum/TopViewController.swift
@@ -7,13 +7,54 @@
 //
 
 import UIKit
+import GoogleMobileAds
 
-class TopViewController: UIViewController {
+class TopViewController: UIViewController, GADBannerViewDelegate {
 
+    // AdMob ID を入れてください
+    let AdMobID = "pub-1674810718316989"
+    let TEST_DEVICE_ID = "61b0154xxxxxxxxxxxxxxxxxxxxxxxe0"
+    let AdMobTest:Bool = true
+    let SimulatorTest:Bool = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        var admobView: GADBannerView = GADBannerView()
+        admobView = GADBannerView(adSize:kGADAdSizeBanner)
+        admobView.frame.origin = CGPointMake(0, self.view.frame.size.height - admobView.frame.height)
+        
+        admobView.frame.size = CGSizeMake(self.view.frame.width, admobView.frame.height)
+        admobView.adUnitID = AdMobID
+        admobView.delegate = self
+        admobView.rootViewController = self
+        
+        let admobRequest:GADRequest = GADRequest()
+        
+        if AdMobTest {
+            if SimulatorTest {
+                admobRequest.testDevices = [kGADSimulatorID]
+            }
+            else {
+                admobRequest.testDevices = [TEST_DEVICE_ID]
+            }
+            
+        }
+        
+        admobView.loadRequest(admobRequest)
+        
+        self.view.addSubview(admobView)
 
-        // Do any additional setup after loading the view.
+//        // Do any additional setup after loading the view.
+//        
+//        let bannerView:GADBannerView = GADBannerView(adSize: kGADAdSizeSmartBannerPortrait)
+//        bannerView.adUnitID = "ca-app-pub-1674810718316989"
+//        bannerView.delegate = self
+//        bannerView.rootViewController = self
+//        let gadRequest:GADRequest = GADRequest()
+//        gadRequest.testDevices = [kGADSimulatorID]  // テスト時のみ
+//        bannerView.loadRequest(gadRequest)
+//        self.view.addSubview(bannerView)
     }
 
     override func didReceiveMemoryWarning() {
